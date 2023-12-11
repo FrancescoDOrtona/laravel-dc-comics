3cad6cf07d2c7ecbe07ef4830d007a07c1e43645
FrancescoDOrtona <francescod.97@hotmail.it>
2023-12-11 17:33:45 +0100

    Add: buy comics section+ scss

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -16,4 +16,30 @@
             </div>
         </div>
     </section>
+    <section class="buy-comics">
+        <div class="container">
+            <div class="buy-comics_structure">
+                <div class="buy-comics_card">
+                    <img src="{{ Vite::asset('resources/images/buy-comics-digital-comics.png') }}" alt="">
+                    <p>digital comics</p>
+                </div>
+                <div class="buy-comics_card">
+                    <img src="{{ Vite::asset('resources/images/buy-comics-merchandise.png') }}" alt="">
+                    <p>dc merchandise</p>
+                </div>
+                <div class="buy-comics_card">
+                    <img src="{{ Vite::asset('resources/images/buy-comics-subscriptions.png') }}" alt="">
+                    <p>subscription</p>
+                </div>
+                <div class="buy-comics_card">
+                    <img src="{{ Vite::asset('resources/images/buy-comics-shop-locator.png') }}" alt="">
+                    <p>comic shop locator</p>
+                </div>
+                <div class="buy-comics_card">
+                    <img src="{{ Vite::asset('resources/images/buy-dc-power-visa.svg') }}" alt="">
+                    <p>dc power visa</p>
+                </div>
+            </div>
+        </div>
+    </section>
 @endsection
