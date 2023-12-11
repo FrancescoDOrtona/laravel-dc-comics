134ed6c40efa22bb28c443e3e9802c9b29e1f47a
FrancescoDOrtona <francescod.97@hotmail.it>
2023-12-11 14:08:23 +0100

    Add: layout page with include, extends & content in header & footer

---
+++ resources/views/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <h1>Page Content</h1>
+    </div>
+@endsection
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,3 @@
+<div class="container">
+    <h1>Page Header</h1>
+</div>
