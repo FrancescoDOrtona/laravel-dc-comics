19fe1852cbde9d1c7b929676770372132bd19ec1
FrancescoDOrtona <francescod.97@hotmail.it>
2023-12-11 18:06:30 +0100

    Add: footer content

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -3,43 +3,48 @@
         <div class="row">
             <div class="col-2">                
                 <ul>
-                    <li>DC COMICS</li>
+                    <li class="text-uppercase pb-2 fw-bold fs-4">dc comics</li>
+                    <li>Characters</li>
                     <li>Comics</li>
-                    <li>Comics</li>
-                    <li>Comics</li>
-                    <li>Comics</li>
-                    <li>Comics</li>
-                    <li>Comics</li>
-                    <li>Comics</li>
+                    <li>Movies</li>
+                    <li>TV</li>
+                    <li>Games</li>
+                    <li>Videos</li>
+                    <li class="pb-2">News</li>
+                    <li class="text-uppercase pb-2 fw-bold fs-4">shop</li>
+                    <li>Shop DC</li>
+                    <li>Shop DC Collectibles</li>
                 </ul>
             </div>
             <div class="col-2">
                 <ul>
-                    <li>DC COMICS</li>
-                    <li>Comics</li>
-                    <li>Comics</li>
-                    <li>Comics</li>
-                    <li>Comics</li>
-                    <li>Comics</li>
-                    <li>Comics</li>
-                    <li>Comics</li>
+                    <li class="text-uppercase pb-2 fw-bold fs-4">DC</li>
+                    <li>Terms of Use</li>
+                    <li>Privacy Policy (New)</li>
+                    <li>Ad Choices</li>
+                    <li>Advertising</li>
+                    <li>Jobs</li>
+                    <li>Subscriptions</li>
+                    <li>Talent Workshop</li>
+                    <li>CPSC Certificates</li>
+                    <li>Ratings</li>
+                    <li>Shop Help</li>
+                    <li>Contact Us</li>
                 </ul>
             </div>
             <div class="col-2">
                 <ul>
-                    <li>DC COMICS</li>
-                    <li>Comics</li>
-                    <li>Comics</li>
-                    <li>Comics</li>
-                    <li>Comics</li>
-                    <li>Comics</li>
-                    <li>Comics</li>
-                    <li>Comics</li>
+                    <li class="text-uppercase pb-2 fw-bold fs-4">sites</li>
+                    <li>DC</li>
+                    <li>MAD Magazine</li>
+                    <li>DC Kids</li>
+                    <li>DC Universe</li>
+                    <li>DC Power Visa</li>
                 </ul>
             </div>            
         </div>       
             <div class="row">
-                <div class="col-5 footer-copy">
+                <div class="col-6 footer-copy">
                     <small>All Site Content TM and &copy; 2020 DC Entertainment, unless otherwise <span class="text-primary">noted here</span>. Allrights reserved. <span class="text-primary">Cookies Settings</span></small>
                 </div>
             </div>        
