99140738fa2e84ec40f10a1923048d23c8c5d449
FrancescoDOrtona <francescod.97@hotmail.it>
2023-12-11 17:53:45 +0100

    Add: footer bg img & row-col structure raw

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,47 @@
-<div class="container">
-    <h1>Page Footer</h1>
-</div>
+<footer class="footer" style="background-image: url({{ Vite::asset('resources/images/footer-bg.jpg') }})">
+    <div class="container">
+        <div class="row">
+            <div class="col-2">                
+                <ul>
+                    <li>DC COMICS</li>
+                    <li>Comics</li>
+                    <li>Comics</li>
+                    <li>Comics</li>
+                    <li>Comics</li>
+                    <li>Comics</li>
+                    <li>Comics</li>
+                    <li>Comics</li>
+                </ul>
+            </div>
+            <div class="col-2">
+                <ul>
+                    <li>DC COMICS</li>
+                    <li>Comics</li>
+                    <li>Comics</li>
+                    <li>Comics</li>
+                    <li>Comics</li>
+                    <li>Comics</li>
+                    <li>Comics</li>
+                    <li>Comics</li>
+                </ul>
+            </div>
+            <div class="col-2">
+                <ul>
+                    <li>DC COMICS</li>
+                    <li>Comics</li>
+                    <li>Comics</li>
+                    <li>Comics</li>
+                    <li>Comics</li>
+                    <li>Comics</li>
+                    <li>Comics</li>
+                    <li>Comics</li>
+                </ul>
+            </div>            
+        </div>       
+            <div class="row">
+                <div class="col-5 footer-copy">
+                    <small>All Site Content TM and &copy; 2020 DC Entertainment, unless otherwise <span class="text-primary">noted here</span>. Allrights reserved. <span class="text-primary">Cookies Settings</span></small>
+                </div>
+            </div>        
+    </div>
+</footer>
