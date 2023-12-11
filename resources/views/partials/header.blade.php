3dd63dea20173ad762ace3480bd0b72ac0940468
FrancescoDOrtona <francescod.97@hotmail.it>
2023-12-11 15:13:49 +0100

    Add: header navbar with logo & scss

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -8,5 +8,27 @@
         </div>
     </div>
     <div class="container">
+        <nav class="navbar row">
+            <div class="col-2 navbar_logo">
+                <img src="{{ Vite::asset('resources/images/dc-logo.png') }}" alt="Logo">
+            </div>
+            <div class="col-8">
+                <ul class="text-uppercase navbar_items">
+                    <li><a href="#">characters</a></li>
+                    <li><a href="#">comics</a></li>
+                    <li><a href="#">movies</a></li>
+                    <li><a href="#">tv</a></li>
+                    <li><a href="#">games</a></li>
+                    <li><a href="#">collectibles</a></li>
+                    <li><a href="#">videos</a></li>
+                    <li><a href="#">fans</a></li>
+                    <li><a href="#">news</a></li>
+                    <li><a href="#">shop</a></li>
+                </ul>
+            </div>
+            <div class="col-2">
+                <input type="search" name="searchbar" id="searchbar" placeholder="Search">
+            </div>
+        </nav>
     </div>
 </header>
