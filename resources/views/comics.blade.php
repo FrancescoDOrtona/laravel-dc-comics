7c5ca73ec3a13474df479fb056f47479c2dbc6ac
FrancescoDOrtona <francescod.97@hotmail.it>
2023-12-11 19:08:23 +0100

    Add: comics page current series badge

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -1,9 +1,12 @@
 @extends('layouts.app')
 
 @section('content')
-    <section class="comics">
+    <section class="comics">       
         <div class="container">
             <div class="d-grid comics_structure">
+                <div class="title-badge">
+                    <h4>current series</h4>
+                </div>
                 @foreach ($comics as $comic)
                    <div class="comics_card">
                         <img src="{{ $comic['thumb'] }}" alt="">
@@ -14,9 +17,7 @@
             <div class="d-flex justify-content-center pb-3 ">
                 {{-- <button class="bg-primary fw-bold text-light text-uppercase px-5 py-1 border border-0">load more</button> --}}
                 <nav aria-label="Page navigation">
-                    <ul
-                        class="pagination    "
-                    >
+                    <ul class="pagination">
                         <li class="page-item disabled">
                             <a class="page-link" href="#" aria-label="Previous">
                                 <span aria-hidden="true">&laquo;</span>
