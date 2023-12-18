379a4137caa3160aa2cc6eb4c856971ded231163
FrancescoDOrtona <francescod.97@hotmail.it>
2023-12-18 16:49:35 +0100

    Add: Form view content & show view content

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -1,17 +1,19 @@
 @extends('layouts.app')
 
 @section('content')
-    <section>
+    <section class="py-5">
         <div class="container">
             <div class="row">               
-                <div class="col-3">
+                <div class="col-4">
                     <img src="{{ $comic->thumb }}" width="300" alt="{{ $comic->title }}">
                 </div>
-                <div class="col-6">
-                    <h1>{{ $comic->title }}</h1>                   
+                <div class="col-8">
+                    <h1 class="pb-4">{{ $comic->title }}</h1>                   
                     <p>{{ $comic->description }}</p>
-                    <p>Prezzo: {{ $comic->price }}</p>
+                    <p>Prezzo: {{ $comic->price }} &dollar;</p>
                     <p>Serie: {{ $comic->series }}</p>
+                    <p>Data di vendita: {{ $comic->sale_date }}</p>
+                    <p>Tipo di fumetto: {{ $comic->type }}</p>
                 </div>                
             </div>
         </div>
--- resources/views/partials/form.blade.php
+++ resources/views/partials/form.blade.php
@@ -1,10 +1,38 @@
 
     <div class="mb-3">
-        <label for="exampleFormControlInput1" class="form-label">Email address</label>
-        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
-      </div>
-      <div class="mb-3">
-        <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
-        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-      </div>
-      <button class="btn btn-primary" type="submit">Salva</button>
+        <label for="title" class="form-label">Title</label>
+        <input type="text" class="form-control" name="title" id="title" placeholder="Titolo del Fumetto">
+    </div>
+
+    <div class="mb-3">
+        <label for="thumb" class="form-label">Thumb</label>
+        <input type="text" class="form-control" name="thumb" id="thumb" placeholder="URL dell'immagine">
+    </div>
+
+    <div class="mb-3">
+        <label for="description" class="form-label">Description</label>
+        <textarea class="form-control" name="description" id="description" rows="3"></textarea>
+    </div>
+
+    <div class="input-group mb-3">
+        <span class="input-group-text">$</span>
+        <input type="text" class="form-control" name="price" aria-label="Amount (to the nearest dollar)">
+        <span class="input-group-text">.00</span>
+    </div>
+
+    <div class="mb-3">
+        <label for="series" class="form-label">Series</label>
+        <input type="text" class="form-control" name="series" id="series" placeholder="Serie">
+    </div>
+
+    <div class="mb-3">
+        <label for="date" class="form-label">Sale Date</label>
+        <input type="date" class="form-control" name="sale_date" id="date">
+    </div>
+
+    <div class="mb-3">
+        <label for="type" class="form-label">Type</label>
+        <input type="text" class="form-control" name="type" id="type" placeholder="Tipologia del Fumetto">
+    </div>
+
+    <button class="btn btn-primary" type="submit">Salva</button>
